7435967d28e6fb24353bfb390bc270aad491499f
Martin <Martin@DESKTOP-I5F6D88.GLASTONBURYUS.ORG>
2018-01-13 11:05:40 -0500

    Initial test file TestMe.m

---
+++ TestMe.m
@@ -0,0 +1,12 @@
+%
+%   TestMe.m
+%
+%   Martin Krucinski
+%   FRC Team 2170 Mentor
+%   2018-01-13
+%
+%   Test program for Matlab
+
+disp([ 'e = ' num2str(exp(1)) ])
+
+Ainv    = inv([ 1 2 3 ; 6 5 4 ; 8 7 9 ])
