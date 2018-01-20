37727feddb8a310e4b8dfd1089f9a93439280547
Martin <Martin@DESKTOP-I5F6D88.GLASTONBURYUS.ORG>
2018-01-20 10:57:57 -0500

    Updates to init_Constants.m

--- Autonomy/init_Constants.m
+++ Autonomy/init_Constants.m
@@ -8,4 +8,7 @@ global deg in ft
 
 deg		= pi/180;		% [rad]		Conversion factor deg->rad
 in		= 0.0254;		% [m]		Conversion factor in->m
-ft      = 0.305;        % [ft]      Conversion factor ft->m
+ft      = 0.305;        % [m]      Conversion factor ft->m
+
+lb      = 0.454;        % [kg]       Conversion factor lb->kg
+oz      = lb/16;        % [kg]       Conversion factor oz->kg
