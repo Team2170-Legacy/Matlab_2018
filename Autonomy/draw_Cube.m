83d09c6e97221b899fe25b815b1f318e1c3d469c
GEH-Magnet School 1 <GEH-Magnet School 1@10.202.1.161>
2018-01-23 19:33:57 -0500

    Autolines and plates updates

---
+++ Autonomy/draw_Cube.m
@@ -0,0 +1,5 @@
+function y=draw_Cube( x, y, Field)
+
+plot(x,y,'*')
+plot(x+Field.L, y, 'r*')
+
---
+++ Autonomy/draw_StartingPositions.m
@@ -0,0 +1,8 @@
+%Plot starting positions
+ARSL = plot(Field.RSL.x, Field.RSL.y, 'rx');
+ARSM = plot(Field.RSM.x, Field.RSM.y, 'rx');
+ARSR = plot(Field.RSR.x, Field.RSR.y, 'rx');
+
+ABSL = plot(Field.BSL.x, Field.BSL.y, 'bx');
+ABSM = plot(Field.BSM.x, Field.BSM.y, 'bx');
+ABSR = plot(Field.BSR.x, Field.BSR.y, 'bx');
