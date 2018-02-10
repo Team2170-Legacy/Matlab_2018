06511a9b20e8000f923806207774d3d5a6c87d89
Adonis0903 <adoniscanada24@gmail.com>
2018-02-10 12:09:30 -0500

    Movie directory added

--- Autonomy/generate_all_Trajectories_v001.m
+++ Autonomy/generate_all_Trajectories_v001.m
@@ -32,12 +32,9 @@ for traj = 1:length(all_Trajectories)
         %   copy movie file
         
         movie_file_name = [ 'Robot_Movie_' trajString '.mp4' ];
-        make_dot_h_file_v002(trajString ,Robot.wR_all,Robot.wL_all,all_t,Robot,t_auto_end, i_auto_end);
-        
-        copy_command    = ['!copy Robot_Movie.mp4 ' 'Movies\' movie_file_name ];
-        
+        make_dot_h_file_v002(trajString ,Robot.wR_all,Robot.wL_all,all_t,Robot,t_auto_end, i_auto_end)
+        copy_command    = ['!copy Robot_Movie.mp4 ' 'Movies\' movie_file_name];
         eval(copy_command);
-        
         close all;
         
        %Init_Robot_v002
