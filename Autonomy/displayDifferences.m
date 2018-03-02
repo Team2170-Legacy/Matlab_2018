a9d594243f95216b3680dee4ba7a14a621afcac5
Adonis0903 <adoniscanada24@gmail.com>
2018-03-02 18:40:35 -0500

    Added programs that calculate starting positions and trajectory distances

--- Autonomy/displayDifferences.m
+++ Autonomy/displayDifferences.m
@@ -1,42 +1,50 @@
 all_Trajectories = {
-   Arc;
-   Straight;
-     BSML;
-%     BSMLS;
-     BSMR;
-%     BSMRS;
- RSML;
-%     RSMLS;
- RSMR;
-%     RSMRS;
-};
+    Arc;
+    Straight;
+    BSML;
+    %     BSMLS;
+    BSMR;
+    %     BSMRS;
+    RSML;
+    %     RSMLS;
+    RSMR;
+    %     RSMRS;
+    };
 
 
 all_Trajectories_String = {
-   'Arc'
-   'Straight'
+    'Arc'
+    'Straight'
     'BSML'
- %   'BSMLS'
+    %   'BSMLS'
     'BSMR'
- %   'BSMRS'
+    %   'BSMRS'
     'RSML'
- %   'RSMLS'
+    %   'RSMLS'
     'RSMR'
-   % 'RSMRS'
+    % 'RSMRS'
     };
 
+changeinx = [];
+changeiny = [];
 for traj = 1:length(all_Trajectories)
-     
+    
     empty = " ";
     trajectory  = all_Trajectories{traj};
-     trajString = all_Trajectories_String{traj};
+    trajString = all_Trajectories_String{traj};
     trajectoryxtravel = trajectory.x(length(trajectory.x)) - trajectory.x(1);
     trajectoryytravel = trajectory.y(length(trajectory.y)) - trajectory.y(1);
     
-   trajString
-   trajectoryxtravel/ft
-   trajectoryytravel/ft
-   disp (empty)
-   
+   % trajString
+    changeinx(traj) =  trajectoryxtravel/ft;
+    changeiny(traj) =   trajectoryytravel/ft;
+%     changeinx(traj)
+%     changeiny(traj)
+    
+%    disp (empty)
+    
 end
 
+for traj = 1:length(all_Trajectories),
+    disp([ all_Trajectories_String{traj} char(9) roundNumber(2,changeinx(traj)) char(9) roundNumber(2,changeiny(traj))]);
+end
