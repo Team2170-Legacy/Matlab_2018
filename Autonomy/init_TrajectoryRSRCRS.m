2f8da66106b031dddab283adadfa41880edac07c
Adonis0903 <adoniscanada24@gmail.com>
2018-03-20 19:19:06 -0400

    Fixed corner trajectories

--- Autonomy/init_TrajectoryRSRCRS.m
+++ Autonomy/init_TrajectoryRSRCRS.m
@@ -1,7 +1,7 @@
 
 
-RSRCRS.x = [Field.RSRC.x, (Field.RSwitch.RightP.tr_x + Field.RSwitch.RightP.tl_x)/2 + RSRCRS.add_x, (Field.RSwitch.RightP.tr_x + Field.RSwitch.RightP.tl_x)/2 + RSRCRS.add_x];
-RSRCRS.y = [Field.RSRC.y, Field.RSRC.y, Field.RSwitch.RightP.br_y-Robot.L/2 + RSRCRS.add_y];
+RSRCRS.x = [Field.RSRC.x, Field.RSRC.x + Robot.L/2, (Field.RSwitch.RightP.tr_x + Field.RSwitch.RightP.tl_x)/2 + RSRCRS.add_x, (Field.RSwitch.RightP.tr_x + Field.RSwitch.RightP.tl_x)/2 + RSRCRS.add_x];
+RSRCRS.y = [Field.RSRC.y, Field.RSRC.y, Field.RSRC.y-.35, Field.RSwitch.RightP.br_y-Robot.L/2 + RSRCRS.add_y];
 RSRCRS.v = 2.0;
 %RSRCRS.t_final = traj_length(RSRCRS) / RSRCRS.v  * 1.3;
 RSRCRS.t_final = traj_length(RSRCRS) / RSRCRS.v  * 1.3;
